fa124f2afe87eb267761652ceb45edd37f2234e3
marcelo <hemakesnoise@gmail.com>
2013-10-25 16:30:51 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reimburser::Application.config.secret_token = 'c98530f6962ffedf03b1be3991367bef570b434bef956c83fc5d9bd58feed42e84c5a08cf9349ec9341e4875a4888b62e60f6ec8037e441a03d11cbc7b9f5039'
